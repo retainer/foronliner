219b71260b367571cd9da864c73aa4b7ee0eb574
Dmitry <device@rambler.ru>
2009-12-23 21:03:13 +0300

    исправлена ошибка при групповом удалении файлов

--- groupcorrection.php
+++ groupcorrection.php
@@ -1,13 +1,16 @@
-﻿<?
+<?
 require "auth.php";
 $uploads_dir = 'uploaduserfiles/';
 $uID=$_SESSION['user_id'];
 //по соображениям безопасности будем проводить проверку для каждой цепочки 'пользователь'-'файл'.
 
 // определим список файлов  переданных для обработки по скрытому полю c именем hidX.
+print_r ($_POST);
+
 foreach ( $_POST as $key => $val )
 {
  $$key = $val;
+
  if (substr($key, 0,3)=="hid")
  {
 	$currentID=substr($key, 3); // рабочий идентификатор
@@ -25,10 +28,9 @@ if (!isset($$CurComm))
 $CurComm=0;
 }
 else $CurComm=1;
-
 	$query = "UPDATE upload_files SET comments_enabled=$CurComm  WHERE user_id=$uID  AND file_id=$currentID";  
     $result = mysql_query($query) or die("БД- ошибка запроса: " . mysql_error());
-}
+
 // произведём групповое удаление файлов с проверкой подлиности пользователя для каждого файла	
 $CurDel="del$currentID";
 	if ($$CurDel=="on")
@@ -48,7 +50,8 @@ $CurDel="del$currentID";
 				}
 				}
 	}
-	} 
+}
+} 
 // вернёмся к списку
 header("Location: http://".$_SERVER['HTTP_HOST']."/list.php");
 ?>
