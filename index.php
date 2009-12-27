cd07ed6bc9577ba67cd3e59c2ed90c7d7e65f300
Dmitry <device@rambler.ru>
2009-12-27 22:35:24 +0300

    создан во избежания доступа к директории с файлами

---
+++ index.php
@@ -0,0 +1,9 @@
+<?
+print "
+Регистрация нового пользователя: <a href='registration.html'>registration.html</a><br>
+Перейти к списку файлов: <a href='listnonregister.php'>listnonregister.php</a><br>
+Перейти к управлению личными файлами <a href='list.php'>list.php</a>
+		";
+
+
+?>
